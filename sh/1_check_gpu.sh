dafbcfbe31c8ded30ade82ba9a288cc6c0b2257e
project2you <project2you@gmail.com>
2024-05-05 19:49:15 +0700

    Update 1_check_gpu.sh

--- sh/1_check_gpu.sh
+++ sh/1_check_gpu.sh
@@ -147,6 +147,15 @@ sudo systemctl restart docker
 
 echo "NVIDIA driver and nvidia-docker2 installation completed."
 
+#ติดตั้ง Nvidia Runtime
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+
+sudo apt-get install -y nvidia-container-toolkit
 
 # ตั้งค่า Docker เพื่อใช้ NVIDIA runtime เป็นค่ามาตรฐาน
 sudo mkdir -p /etc/docker
