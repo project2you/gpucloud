88ebb6331247356a30f0f2c47efaff50b8957156
project2you <project2you@gmail.com>
2024-05-06 02:06:29 +0700

    Update 1_check_driver.sh

--- client/1_check_driver.sh
+++ client/1_check_driver.sh
@@ -122,6 +122,11 @@ else
     echo "No need to install CUDA."
 fi
 
+#Cuda
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda
 
 # เพิ่ม GPG key และติดตั้ง nvidia-docker
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
