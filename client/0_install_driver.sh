2dce860e3db49180692fa87c6b32605ec02f59b9
project2you <project2you@gmail.com>
2024-05-06 20:30:22 +0700

    Update 0_install_driver.sh

--- client/0_install_driver.sh
+++ client/0_install_driver.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Define the file name
+FILE_NAME="1_check_driver.sh"
+
+# Check if the file already exists
+if [ -f "$FILE_NAME" ]; then
+    rm -f "$FILE_NAME"
+fi
+
 # URL of the script
 SCRIPT_URL="https://raw.githubusercontent.com/project2you/gpuspeed.net/main/client/1_check_driver.sh"
+
 
 # Download the script using curl
 curl -O $SCRIPT_URL
@@ -14,3 +23,5 @@ chmod +x $SCRIPT_NAME
 
 # Execute the script
 ./$SCRIPT_NAME
+
+rm -f "$FILE_NAME"
